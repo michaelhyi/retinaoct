c21ec64fe79b1a740d58adab5af3b44e7d5b8179
23yimichael <23yimichael@gmail.com>
2022-06-20 18:38:33 -0700

    implemented tensorflow

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,11 +1,17 @@
-const { getDefaultConfig } = require("metro-config");
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
-  const { assetExts } = defaultConfig.resolver;
-  return {
-    resolver: {
-      // Add bin to assetExts
-      assetExts: [...assetExts, "bin"],
-    },
-  };
-})();
+const blacklist = require("metro-config/src/defaults/exclusionList");
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    assetExts: ["bin", "txt", "jpg", "ttf", "png"],
+    sourceExts: ["js", "json", "ts", "tsx", "jsx"],
+    blacklistRE: blacklist([/platform_node/]),
+  },
+};
